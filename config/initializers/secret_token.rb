e2fb6a5ed38dff8d3039d72e93f25cc91527b0c6
Alex Berry <alex@apprentice.io>
2012-03-27 23:38:28 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roboterror::Application.config.secret_token = '858f9493990176b41e7b4dbc1099903931ff9886361f89d235146f3d1a818185dee246b4c156eef34ff44ea13dcbe8cd3e05f5a53a4090f37113243c019010fb'
